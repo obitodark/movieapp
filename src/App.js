99d5cbe7e47c1258b9c6fe58eeb13e552d161fd7
willians ccallo <mayhixccallo@gmail.com>
2022-08-08 19:24:03 -0500

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { AuthProvider, MovieFavoriteProvider } from "./context";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import Router from "./router";
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<Router />
-		</ThemeProvider>
-	);
+  return (
+    <AuthProvider>
+      <MovieFavoriteProvider>
+        <ThemeProvider theme={theme}>
+          <Router />
+        </ThemeProvider>
+      </MovieFavoriteProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainView } from "../pages";
+import {
+	LoginView,
+	SearchView,
+	SearchResultsView,
+	FavoritesView,
+} from "../pages";
+import { MainLayout } from "../layouts";
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<MainView />} />
+				<Route path="/" element={<LoginView />} />
+				<Route element={<MainLayout />}>
+					<Route path="/search" element={<SearchView />} />
+					<Route
+						path="/search/:searchText"
+						element={<SearchResultsView />}
+					/>
+					<Route path="/favorites" element={<FavoritesView />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
